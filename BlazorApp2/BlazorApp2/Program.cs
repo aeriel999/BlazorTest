c34da0a082b6f2b95479c36907b31e99b2ded4e5
aeriel999 <ok4743@gmail.com>
2024-12-07 08:59:41 +0200

    Work with DB

--- BlazorApp2/BlazorApp2/Program.cs
+++ BlazorApp2/BlazorApp2/Program.cs
@@ -1,7 +1,16 @@
-using BlazorApp2.Client.Pages;
+﻿using BlazorApp2.Client.Pages;
 using BlazorApp2.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BlazorApp2.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<BlazorApp2Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlazorApp2Context") ?? throw new InvalidOperationException("Connection string 'BlazorApp2Context' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -20,6 +29,7 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
